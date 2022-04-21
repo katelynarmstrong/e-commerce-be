4faa8aeadb21ba207022c3540ee1a2f5598e5d5c
katelynarmstrong <katelynarmstrong6@gmail.com>
2022-04-20 20:09:55 -0500

    tag routes updated

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
 const { Tag, Product, ProductTag } = require("../../models");
 
-// The `/api/tags` endpoint
-
 router.get("/", (req, res) => {
   Tag.findAll({
     include: [
@@ -43,15 +41,44 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  // create a new tag
+  Tag.create(req.body)
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(req.body, { where: { id: req.params.id } })
+    .then((data) => {
+      if (!data[0]) {
+        res.status(404).json({ message: "No category found with this id" });
+        return;
+      }
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({ where: { id: req.params.id } })
+    .then((data) => {
+      if (!data) {
+        res.status(404).json({ message: "No tag found with this id" });
+        return;
+      }
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
